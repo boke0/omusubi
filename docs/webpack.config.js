10840cfbad375adfb307350c2d37e5603b3c2dcc
木瓜丸 <boke0@eugensite.com>
2020-12-08 15:14:15 +0900

    [update]

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -1,5 +1,4 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
   entry: './src/main.js',
@@ -36,7 +35,6 @@ module.exports = {
     port: 3031
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin(),
     new HtmlWebpackPlugin({
       inject: true,
       template: `${__dirname}/index.html`
